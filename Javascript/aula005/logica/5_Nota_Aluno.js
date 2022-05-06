c91c71dd8ebcb95ba56cff2b3cc080da725e25ed
Eric <yuukisouma2003@gmail.com>
2022-05-06 00:07:14 -0300

    Adicionado coméntarios

--- Javascript/aula005/logica/5_Nota_Aluno.js
+++ Javascript/aula005/logica/5_Nota_Aluno.js
@@ -1,7 +1,9 @@
+// Definido nota do aluno (3 notas)
 const notaPeso1 = 5
 const notaPeso2 = 7
 const notaPeso3 = 3
 
+// Criando um objeto para guardar nome e notas com o calculo do peso da nota.
 const notasAluno = {
     Nome: 'Eric',
     Nota1: notaPeso1 * 2,
@@ -9,5 +11,6 @@ const notasAluno = {
     Nota3: notaPeso3 * 5
 }
 
+// Fazendo calculo da nota final e mostrando no console.
 const notaFinal = (notasAluno.Nota1 + notasAluno.Nota2 + notasAluno.Nota3) / 10
 console.log(`A nota final do aluno ${notasAluno.Nome} é ${notaFinal}`) 

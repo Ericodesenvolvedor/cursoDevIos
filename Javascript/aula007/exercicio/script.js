6b10ad8e0b2758cec2d3660fa6fb0b202a91b376
Eric <yuukisouma2003@gmail.com>
2022-05-12 12:06:56 -0300

    Exercicio do array de colaboradores - aula 07

--- Javascript/aula007/exercicio/script.js
+++ Javascript/aula007/exercicio/script.js
@@ -7,43 +7,96 @@ const colaboradores = [
 
     {
         nome: 'Eric',
-        salario: 1300,
+        salario: 1200,
         vinculo: 'CLT',
-        valorInss: 
+        inss: ''
     } ,
 
     {
         nome: 'Renata',
         salario: 3500,
-        vinculo: 'PJ'
+        vinculo: 'PJ',
+        inss: ''
     } ,
 
     {
         nome: 'Felipe',
         salario: 2000,
-        vinculo: 'PJ'
+        vinculo: 'PJ',
+        inss: ''
     } ,
 
     {
         nome: 'Samantha',
         salario: 5600,
-        vinculo: 'CLT'
+        vinculo: 'CLT',
+        inss: ''
     } ,
 
     {
         nome: 'Yuri',
         salario: 1800,
-        vinculo: 'CLT'
+        vinculo: 'CLT',
+        inss: ''
     } ,
 
 ]
 
-const salarioEric = colaboradores[0].salario
-const salarioRenata = colaboradores[1].salario
-const salarioFelipe = colaboradores[2].salario
-const salarioSamantha = colaboradores[3].salario
-const salarioYuri = colaboradores[4].salario
+for (var index = 0;index < colaboradores.length; index++) {
 
-function calcularInss(salario, aliquota, valorFixo) {
+    let salarioColaborador = colaboradores[index].salario;
+    let aliquota;
 
+    // FUNÇÃO PARA CALCULAR O SALÁRIO COM INSS
+    const calcularInss = (salario, porcentagem) => {
+        let inssValor = salario * (porcentagem / 100) + salario
+         
+        // FUNÇÃO PARA DISTRIBUIR VALORES AOS DEVIDOS VINCULOS
+        const checarVinculo = () => {
+            if (colaboradores[index].vinculo == 'PJ') {
+                colaboradores[index].inss = 0
+            } 
+
+            else {
+                colaboradores[index].inss = inssValor
+            }
+        }
+
+        checarVinculo()
+    }
+
+    // CHECANDO SALARIO E CHAMANDO FUNÇÃO.
+    if (salarioColaborador <= 1212) {
+        aliquota = 7.5;
+        calcularInss(salarioColaborador, aliquota)
+    }
+
+    else if (salarioColaborador <= 242735) {
+        aliquota = 9;
+        calcularInss(salarioColaborador, aliquota)
+    }
+
+    else if (salarioColaborador <= 364103) {
+        aliquota = 12;
+        calcularInss(salarioColaborador, aliquota)
+    }
+
+    else if (salarioColaborador <= 708722 || salarioColaborador > 708722) {
+        aliquota = 14;
+        calcularInss(salarioColaborador, aliquota)
+    }
+    
+    // FORMATA O VALOR NUMBER PARA MOEDA(R$)
+    const transformarMoeda = () => {
+        let inss = Number(colaboradores[index].inss).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
+        return inss
+    }
+
+    // EXIBINDO NO CONSOLE
+    if (colaboradores[index].inss != 0) {
+        console.log(`Colaborador(a) ${colaboradores[index].nome} do vinculo ${colaboradores[index].vinculo} vai receber ${transformarMoeda()} por suas contribuições`)
+
+    } else {
+        console.log(`Colaborador(a) ${colaboradores[index].nome} do vinculo ${colaboradores[index].vinculo} não vai receber inss, ou seja contribuições == ${transformarMoeda()}.`)
+    }
 }

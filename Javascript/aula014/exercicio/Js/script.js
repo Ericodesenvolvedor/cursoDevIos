ed20d067930be3444b3cd5fe6121e031c8091525
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2022-05-25 21:47:03 -0300

    manipulando dom, exercicio e pratica

--- Javascript/aula014/exercicio/Js/script.js
+++ Javascript/aula014/exercicio/Js/script.js
@@ -1,56 +1,43 @@
 const nome = document.getElementById('nome');
 const dataNascimento = document.getElementById('dataNascimento');
 const email = document.getElementById('email');
+let divEmail = document.getElementById('div-email');
 let tipoCarta = document.getElementById('tipoCarta');
 const enviarDados = document.getElementById('enviar');
 const dados = document.getElementById('pessoas');
 
-enviarDados.addEventListener('click', enviar);
 
+enviarDados.addEventListener('click', enviar);
+email.addEventListener('change', verificarEmail);
+
+// Função para enviar os dados e guardar
 function enviar(event) {
     event.preventDefault();
 
-    let lista = document.createElement('li');
-    let btnExcluir = document.createElement('button')
-    dados.appendChild(lista);
-    lista.style.backgroundColor = 'white'
-    lista.innerHTML = `Nome: ${nome.value} | Email: ${email.value} | Data de nascimento: ${dataNascimento.value} | Tipo de carta: ${tipoCarta.value}`
-    lista.appendChild(btnExcluir);
-    btnStyle(btnExcluir)
+    let idade = pegarIdade(dataNascimento);
 
-    btnExcluir.addEventListener('click', excluirItem);
+    if (idade < 18) {
+        alert('Você é menor de idade');
+    } else {
+        let lista = document.createElement('li');
+        let btnExcluir = document.createElement('button')
+        dados.appendChild(lista);
+        lista.style.backgroundColor = 'white'
+        lista.innerHTML = `Nome: ${nome.value} | Email: ${email.value} | Data de nascimento: ${dataNascimento.value} | Tipo de carta: ${tipoCarta.value}`
+        lista.appendChild(btnExcluir);
+        btnStyle(btnExcluir)
 
-    function excluirItem() {
-        lista.remove();
-        btnExcluir.remove();
+        btnExcluir.addEventListener('click', excluirItem);
+
+        function excluirItem() {
+            lista.remove();
+            btnExcluir.remove();
+        }
     }
-
-    pegarIdade()
 
     nome.value = ''
     email.value = ''
     dataNascimento.value = ''
-
-    function pegarIdade() {
-        let dataNiver = dataNascimento.value 
-        let semTraco = dataNiver.replace("-", " ");
-        semTraco = semTraco.replace("-", " ")
-        let tranformarEmArray = semTraco.split(" ");
-        
-        let date = new Date();
-        let dia = date.getDate();
-        let mes = date.getMonth() + 1
-        let ano = date.getFullYear();
-
-        // Maior ou Menor de idade
-        
-        if (mes < tranformarEmArray[1] || mes == tranformarEmArray[1] && dia < tranformarEmArray[2]) {
-            console.log('Você já é maior de idade');
-        } else {
-            console.log('Menor de idade');
-        }
-    }
-
 }
 
 function btnStyle(btn) {
@@ -58,4 +45,38 @@ function btnStyle(btn) {
     btn.style.backgroundColor = 'red';
     btn.style.padding = '0.4rem 1.5rem';
     btn.style.marginLeft = '1rem'
+}
+
+function pegarIdade(data) {
+    let nascimento = data.value
+    nascimento = nascimento.replace('-', ' ')
+    nascimento = nascimento.replace('-', ' ')
+    let array = nascimento.split(' ');
+
+    let objetoData = new Date();
+    let dia = objetoData.getDate();
+    let mes = objetoData.getMonth();
+    let ano = objetoData.getFullYear();
+
+    if (mes < array[1] || mes == array[1] && dia < array[2]) {
+        idade = ano - array[0];
+    }
+
+    return --idade
+}
+
+function verificarEmail(e) {
+    let padrao = new RegExp(/.*@.*\..*/i);
+    console.log(padrao);
+
+    if (!padrao.test(email.value)) {
+        // alert('Por favor, insira um e-mail válido.');
+        let erro = document.createElement('p');
+        divEmail.appendChild(erro);
+
+        erro.innerHTML = 'Email invalido';
+        erro.style.color = 'red';
+        setTimeout(() => erro.remove(), 3000);
+        console.log(padrao);
+    }
 }

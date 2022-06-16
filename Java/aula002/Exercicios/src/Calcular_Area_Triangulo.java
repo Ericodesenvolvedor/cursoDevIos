521887edb6ec2b4204dec0d5395d5b8a2bd27d6b
Ericodesenvolvedor <yuukisouma2003@gmail.com>
2022-06-16 18:51:08 -0300

    Exercicios aula 02 Java

---
+++ Java/aula002/Exercicios/src/Calcular_Area_Triangulo.java
@@ -0,0 +1,8 @@
+public class Calcular_Area_Triangulo {
+    public static void main(String[] args) {
+        int baseTriangulo = 10;
+        int alturaTriangulo = 5;
+        float areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+        System.out.println("A area do triângulo é: " + areaTriangulo);
+    }
+}
---
+++ Java/aula002/Exercicios/src/Calcular_Media.java
@@ -0,0 +1,10 @@
+public class Calcular_Media {
+    public static void main(String[] args) {
+        int nota01 = 10;
+        int nota02 = 7;
+        int nota03 = 4;
+        int nota04 = 5;
+        float tirarMedia = (nota01 + nota02 + nota03 + nota04) / 4;
+        System.out.println("A média é: " + tirarMedia);
+    }
+}
